832f3aab66676cdb771cd219fe71c6db2259ec34
huybery <huybery@users.noreply.github.com>
2024-01-26 18:01:31 +0000

    deploy: e9083cbf810cca4ecc440a348b0b6ec20fed4218

---
+++ js/custom.f20a5212619392e989b6d24ad9ce42302014debfad4d3c8c01db030c36d03475.js
@@ -0,0 +1,39 @@
+const navHeight = 60;
+
+/* Transition effect for navbar */
+document.addEventListener('scroll', (e) => {
+    /* checks if window is scrolled more than navHeight px, adds/removes bg-white and shadow class */
+    const nav = document.querySelector('.nav-container');
+    const scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+    if (scrollTop > navHeight) {
+        nav.className = 'nav-container shadow'
+    } else {
+        if (hasHeaderBg) {
+            nav.className = 'nav-container nav-background'
+        } else {
+            nav.className = 'nav-container'
+        }
+    }
+});
+
+/* example gallery */
+document.addEventListener('DOMContentLoaded', function() {
+    const examples = document.querySelectorAll(".example-container");
+    examples.forEach((example)=>{
+        const elements = example.querySelectorAll(".example-content");
+        for (let i=0; i < elements.length; i++) {
+            const ele = elements[i];
+            const nexti = i + 1 === elements.length ? 0: i+1;
+            const next_ele = elements[nexti];
+            const title = ele.querySelector(".title").firstElementChild;
+            const title_text = `${title.textContent} (${i+1}/${elements.length})`
+            title.textContent = title_text;
+            const button = ele.querySelector(".next-button");
+            button.addEventListener("click", (e) => {
+                e.preventDefault();
+                ele.style.display = "none";
+                next_ele.style.display = "block"
+            })
+        }
+    })
+});
